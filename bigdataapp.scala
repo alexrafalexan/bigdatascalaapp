e0ee4ad1c207b59f171fcc3a4454a5e621444842
Alexandridis Alexander <alexrafalexadridis@gmail.com>
2018-06-24 01:47:58 +0300

    NewVersion

--- bigdataapp.scala
+++ bigdataapp.scala
@@ -1,45 +1,65 @@
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.DataFrame
 import scala.io.StdIn.readLine
 import java.io.IOException
 import java.io.FileNotFoundException
-power // Enable power user mode
-var check = false
-while(check == false){
-println("Select the FIRST file:")
-val a = repl.in.readLine("Write full path:")
-println("Select the SECOND file:")
-val b = repl.in.readLine("Write full path:")
-println(s"$b")
-try {
- val  dfa = spark.read.option("header","true").option("inferSchema","true").csv(s"$a")
- println("file 1 ok")
- dfa.show(5)
- check = true
+import scala.util.control.Breaks._
+
+var check: Boolean = false
+var filename1: String = "First"
+var filename2: String = "Second"
+// var dfa: DataFrame = null
+
+def loadfile (file: String): DataFrame = {
+
+      var check = false
+      var dfa:DataFrame = null
+      // Create first dataframe
+      while (check == false) {
+      println(s"Select the $file file:")
+      val a = repl.in.readLine("Write full path:")
+
+      println(s"$a")
+
+
+      try {
+        var dfa:DataFrame = spark.read.option("header","true").option("inferSchema","true").csv(s"$a")
+        println(s"$file file Loaded..")
+        dfa.show(5)
+      //  dfa.schema()
+        check = true
+          return dfa
+      }
+      catch {
+        case ex: Exception => {
+          println("File not found. Please enter correct FIRST file path")
+          check = false
+        }
+      }
+
+    }
+      return dfa
+  }
+
+
+def partition(dfname: DataFrame, file: String): Int ={
+  val part = dfname.rdd.partitions.size
+  println(s"DataFrame $file has $part partitions")
+      println("Do you want to repartition?(y/n)")
+  // var a = repl.in.readLine("Select answer: ")
+  // if ( a == "y") {
+  //   println("Select repartition number")
+  //   var b = repl.in.readLine("Number")
+  // }  else if (a == "n"){
+  //   println("Select repartition number")
+  // } else {
+  //   println("Eisai MALAKAS!!!!")
+  // }
+    return part
 }
-catch {
-case ex: FileNotFoundException => {
-  println("File not found. Please enter correct FIRST file path")
-  check = false
-}
-case ex: Exception => {
-  println("File not found. Please enter correct FIRST file path")
-  check = false
-}
-}
-try {
- val  dfb = spark.read.option("header","true").option("inferSchema","true").csv(s"$b")
- println("file 2 ok")
-  dfb.show(5)
- check = true
-}
-catch {
- case ex2: FileNotFoundException => {
-   println("File not found. Please enter correct SECOND file path")
-   check = false
- }
- case ex2: Exception => {
-   println("File not found. Please enter correct SECOND file path")
-   check = false
- }
-}
-}
+
+  val df1:DataFrame = loadfile(s"$filename1")
+  val df2:DataFrame = loadfile(s"$filename2")
+
+  partition(df1,s"$filename1")
+  partition(df2,s"$filename2")
