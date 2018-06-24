398febc87b868a581ca9d805398f242ae687e411
Alexandridis Alexander <alexrafalexadridis@gmail.com>
2018-06-24 17:25:06 +0300

    Repartition
    choose "y"

--- bigdataapp.scala
+++ bigdataapp.scala
@@ -12,6 +12,8 @@ var filename2: String = "Second"
 
 def loadfile(file: String): DataFrame = {
 
+  val spark = SparkSession.builder().getOrCreate()
+
   var check = false
   var dfa: DataFrame = null
   // Create first dataframe
@@ -41,36 +43,37 @@ def loadfile(file: String): DataFrame = {
   return dfa
 }
 
-
-def partition(dfname: DataFrame, file: String): Int = {
+def repartition(dfname: DataFrame, file: String): DataFrame = {
+  var dftemprepart: DataFrame = null
   var part: Int = dfname.rdd.partitions.size
   var check = true
-    while(check == true){
-      println(s"DataFrame $file has $part partitions")
-      println("Do you want to repartition?(y/n)")
-      var a:String = repl.in.readLine("Select answer: ")
-      if (a.equals("y")) {
-        println("Select repartition number")
-        var b = repl.in.readLine("Number: ")
-        var i = (s"$b").toInt
-        var part = i
-        var check = false
-        return part
-      }else if (a.equals("n")) {
-        println(s"You select $part partitions for $file")
-        var check = false
-        return part
-        } else {
-        println("Eisai MALAKAS!!!!")
-        var check = true
-        }
-      }
-          return part
-        }
+  while(check == true){
+    println(s"DataFrame $file has $part partitions")
+    println("Do you want to repartition?(y/n)")
+    var a:String = repl.in.readLine("Select answer: ")
+    if (a.equals("y")) {
+      println("Select repartition number")
+      var b = repl.in.readLine("Number: ")
+      var part = (s"$b").toInt
+      var dftemprepart = dfname.repartition(part)
+      println(s"You select $part partitions for $file")
+      var check = false
+      return dftemprepart
+    }else if (a.equals("n")) {
+      println(s"You select $part partitions for $file")
+      var check = false
+      return dfname
+    } else {
+      println("Eisai MALAKAS!!!!")
+      var check = true
+    }
+  }
+  return dftemprepart
+}
 
-        val df1: DataFrame = loadfile(s"$filename1")
-        val df2: DataFrame = loadfile(s"$filename2")
+val df1: DataFrame = loadfile(s"$filename1")
+val df2: DataFrame = loadfile(s"$filename2")
 
-        val part1: Int = partition(df1, s"$filename1")
-        println(s"Partition you have selected $part1")
-        //  partition(df2, s"$filename2")
+val df1repart:DataFrame = repartition(df1, s"$filename1")
+val part1 = df1repart.rdd.partitions.size
+println(s"Partition you have selected $part1")
