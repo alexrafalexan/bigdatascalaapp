9a85c0c81e177e05f4e827455863156c62e596b3
Nikos Math <nmathioudis@gmail.com>
2018-06-27 00:24:14 +0300

    Create Points
    Attention to import.sqlContext.implicits._
    positiion

---
+++ magellan.scala
@@ -0,0 +1,36 @@
+import magellan.{Point, Polygon}
+import org.apache.spark.sql.magellan.dsl.expressions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.SparkSession
+import scala.io.StdIn.readLine
+import magellan.coord.NAD83
+import org.apache.spark.sql.magellan._
+import org.apache.spark.sql.magellan.dsl.expressions._
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.DataFrame
+import scala.reflect.runtime.universe._
+
+
+
+
+val spark = SparkSession.builder().getOrCreate()
+val sqlContext= new org.apache.spark.sql.SQLContext(sc)
+import sqlContext.implicits._
+println("Select the A file:")
+
+val a = repl.in.readLine("Write full path:")
+println(s"$a")
+
+case class TaxiRecord(tid: String, timestamp: String, point: Point)
+
+val taxi = sc.textFile(s"$a").map{line =>
+  val parts = line.split(",")
+  val tid = parts(0)
+  val timestamp = parts(1)
+  val point = Point(parts(3).toDouble, parts(2).toDouble)
+  TaxiRecord(tid,timestamp,point)
+}.toDF()
+
+taxi.show()
