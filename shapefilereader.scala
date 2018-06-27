e247e15aac4b982e5ad474b817e6aba86f07ab12
Fordakias <alexrafalexadridis@gmail.com>
2018-06-28 02:00:12 +0300

    Again
    tripId --> tid

--- shapefilereader.scala
+++ shapefilereader.scala
@@ -45,5 +45,5 @@ neighborhoods.show()
 
 neighborhoods.join(taxi).
 where($"point".within($"polygon")).
-select($"tripId", $"timestamp").
+select($"tid", $"timestamp").
 withColumnRenamed("v", "neighborhood").drop("k").show(5)
