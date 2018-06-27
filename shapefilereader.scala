e83ef55cee5713b33ac461c04a570c9517804acc
Fordakias <alexrafalexadridis@gmail.com>
2018-06-28 01:22:25 +0300

    Added

--- shapefilereader.scala
+++ shapefilereader.scala
@@ -19,7 +19,31 @@ val spark = SparkSession.builder().getOrCreate()
 val sqlContext= new org.apache.spark.sql.SQLContext(sc)
 import sqlContext.implicits._
 
+println("Select the A file:")
+
+val a = repl.in.readLine("Write full path:")
+println(s"$a")
+
+case class TaxiRecord(tid: String, timestamp: String, point: Point)
+
+val taxi = sc.textFile(s"$a").map{line =>
+  val parts = line.split(",")
+  val tid = parts(0)
+  val timestamp = parts(1)
+  val point = Point(parts(3).toDouble, parts(2).toDouble)
+  TaxiRecord(tid,timestamp,point)
+}.toDF()
+
+taxi.show()
+
 val neighborhoods = sqlContext.read.format("magellan").
-load("/usr/lib/spark/projects/polygon/Beijing").
+load("/usr/lib/spark/scala_files/planning_neighborhoods/").
 select($"polygon", $"metadata").
-cache()
+cache().toDF()
+
+neighborhoods.show()
+
+neighborhoods.join(taxi).
+where($"point".within($"polygon")).
+select($"tripId", $"timestamp",explode($"metadata").as(Seq("k", "v"))).
+withColumnRenamed("v", "neighborhood").drop("k").show(5)
