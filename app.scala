2d68b3c483e499ee0bdc869c76f728e2a70f8ed0
Nikos Math <nmathioudis@gmail.com>
2018-06-30 15:49:40 +0300

    Appupdated
    Repartition

--- app.scala
+++ app.scala
@@ -21,10 +21,12 @@ import org.apache.hadoop.mapred.InvalidInputException
 import java.lang.ArrayIndexOutOfBoundsException
 import java.lang.IllegalArgumentException
 import org.apache.spark.sql.AnalysisException
+import java.lang.NumberFormatException
 import scala.util.control.Breaks._
 
 
 val spark = SparkSession.builder().getOrCreate()
+
 // equation for csv to df
 def loadfile(file: String): DataFrame = {
   val spark = SparkSession.builder().getOrCreate()
@@ -60,9 +62,9 @@ def loadfile(file: String): DataFrame = {
 //equation for trajectories dataframe
 val sqlContext = new org.apache.spark.sql.SQLContext(sc)
 import sqlContext.implicits._
-case class TaxiRecord(tid: String, timestamp: String, point: Point)
+case class TrRecord(tid: String, timestamp: String, point: Point)
 def loadfiletrajectories (): DataFrame = {
-  val taxiloadfile: DataFrame = null
+  val trloadfile: DataFrame = null
   var check = true
   while (check == true){
     println("Select the A file")
@@ -72,33 +74,33 @@ def loadfiletrajectories (): DataFrame = {
     val splitarg = repl.in.readLine("Write the split argument:")
     println(s"$splitarg")
     try {
-      val taxiloadfile = sc.textFile(s"$a").map{line =>
+      val trloadfile = sc.textFile(s"$a").map{line =>
         val parts = line.split(s"$splitarg")
         val tid = parts(0)
         val timestamp = parts(1)
         val point = Point(parts(2).toDouble, parts(3).toDouble)
-        TaxiRecord(tid,timestamp,point)
+        TrRecord(tid,timestamp,point)
       }.toDF()
       check = false
-      taxiloadfile.show()
-      return taxiloadfile
+      trloadfile.show()
+      return trloadfile
     }
     catch {
       case ex: InvalidInputException =>{
-        println("DEN YPARXEI ARXEIO")
+        println("File does not exist")
         check = true
       }
       case ex: IllegalArgumentException => {
-        println("TO EISAGOMENO DN EINAI MORFIS PATH")
+        println("Insert a path format")
         check = true
       }
       case ex: Exception =>{
-        println("LATHOS DELIMITER")
+        println("Delimeter error")
         check = true
       }
     }
   }
-  return taxiloadfile
+  return trloadfile
 }
 
 //load polygon files
@@ -122,7 +124,7 @@ def loadfolderpolygon (): DataFrame ={
         println("Please enter correct Second file path")
       }
       case ex: IllegalArgumentException => {
-        println("TO EISAGOMENO DN EINAI MORFIS PATH")
+        println("Insert a path format")
         check = true
       }
     }
@@ -130,26 +132,75 @@ def loadfolderpolygon (): DataFrame ={
   return polygonsDF
 }
 
+//repartition
+def repartition(dfname: DataFrame, file: String): DataFrame = {
+  var dftemprepart: DataFrame = null
+  var part: Int = dfname.rdd.partitions.size
+  var check = true
+  while(check == true){
+    println(s"$file DataFrame has $part partitions")
+    println("Do you want to repartition?(y/n)")
+    var a:String = repl.in.readLine("Select answer: ")
+    if (a.equals("y")) {
+      var checkpartition = true
+      while(checkpartition == true){
+        println("Select repartition number")
+        try {
+          var part = (repl.in.readLine("Number: ")).toInt
+          var dftemprepart = dfname.repartition(part)
+          println(s"You select $part partitions for $file")
+          checkpartition = false
+          return dftemprepart
+        } catch {
+          case e: IllegalArgumentException => {
+            println("Enter correct partition number")
+            checkpartition = true
+          }
+        }
+      }
+      return dftemprepart
+      check = false
+    }else if (a.equals("n")) {
+      println(s"You will keep $part partitions for $file")
+      var check = false
+      return dfname
+    } else {
+      println("Select y or n")
+      var check = true
+    }
+  }
+  return dftemprepart
+}
 
-// load files
-var check2: Boolean = true
-while (check2 == true) {
-  println(s"Select the type of files you want to join")
-  var fileTypeSelection: Int = (repl.in.readLine("(0-->trajectories with trajectories, 1-->trajectories with polygon file):")).toInt
-  if (fileTypeSelection == 0) {
-    var first: String = "First"
-    var second: String = "Second"
-    val file1: DataFrame = loadfile(s"$first")
-    val file2: DataFrame = loadfile(s"$second")
-    check2 = false
-  }
-  else if (fileTypeSelection == 1) {
-    val file1: DataFrame = loadfiletrajectories()
-    val file2: DataFrame = loadfolderpolygon()
-    check2 = false
-  }
-  else {
-    println("Select 0 or 1")
-    check2 == true
+// main
+def main(): Unit = {
+  println("Hello from main of class")
+  var check2: Boolean = true
+  var first: String = "First"
+  var second: String = "Second"
+  while (check2 == true) {
+    println(s"Select the type of files you want to join")
+    var fileTypeSelection: Int = (repl.in.readLine("(0-->trajectories with trajectories, 1-->trajectories with polygon file):")).toInt
+    if (fileTypeSelection == 0) {
+      val file1: DataFrame = loadfile(s"$first")
+      val file2: DataFrame = loadfile(s"$second")
+      check2 = false
+      val file1partioned: DataFrame = repartition(file1,s"$first")
+      val file2partioned: DataFrame = repartition(file2,s"$second")
+    }
+    else if (fileTypeSelection == 1) {
+      val file1: DataFrame = loadfiletrajectories()
+      val file2: DataFrame = loadfolderpolygon()
+      check2 = false
+      val file1partioned: DataFrame = repartition(file1,s"$first")
+      val file2partioned: DataFrame = repartition(file2,s"$second")
+    }
+    else {
+      println("Select 0 or 1")
+      check2 == true
+    }
   }
 }
+
+//call main
+val aaa = main()
